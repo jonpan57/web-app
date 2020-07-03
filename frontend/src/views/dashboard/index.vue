306fa84e4977e4067b533484839258d1f62b9bab
manjaro <jonpan57@gmail.com>
2020-07-03 17:33:49 +0800

    initial commit

--- frontend/src/views/dashboard/index.vue
+++ frontend/src/views/dashboard/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+    {{ link }}
+    <br>
+    <a :href="link">百度</a>
+    <br>
+    <span v-html="myLink" />
+    <br>
+    <span v-text="myLink" />
+    <br>
+    <input type="text" @keyup.enter="addbtn">
+    {{ count }}
+    <button type="button" @click="addbtn()">add</button>
   </div>
 </template>
 
@@ -9,10 +20,23 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      count: 0,
+      link: 'www.baidu.com',
+      myLink: '<a href="www.gamersky.com">游民</a>'
+    }
+  },
+
   computed: {
     ...mapGetters([
       'name'
     ])
+  },
+  methods: {
+    addbtn: function() {
+      this.count += 1
+    }
   }
 }
 </script>
